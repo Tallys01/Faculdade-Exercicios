9b0d8ab0a77e445ff18df3870de50f2199558e02
Mateus Tallys <mateustallys0101@gmail.com>
2020-02-19 13:46:12 -0300

    Exercicio C (Aula 1)

---
+++ Ex_C_Aula_1.py
@@ -0,0 +1,4 @@
+primeira_nota = int(input("Informe a primeira Nota: "))
+segunda_nota = int(input("Informe a segunda Nota: "))
+resultado = (primeira_nota + segunda_nota) / 2
+print(f"A media do Aluno(a) é {resultado}")
